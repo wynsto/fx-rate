4c17ce24e574a7d5a0e6efba79e1ff24cc2826be
Wynston <houyewei@gmail.com>
2023-05-16 20:55:11 +0800

    sync code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {Rate} from './Rate'
 import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
-const currencies = ['hkd', 'usd', 'cny', 'eur']
+const currencies = ['hkd', 'usd', 'cny', 'eur', 'aud', 'cad', 'gbp', 'jpy', 'sgd', 'thb']
 
 function App() {
   const [quotes, setQuotes] = useState({})
+  const [base, setBase] = useState(0)
   useEffect(() => {
     fetch('https://houyewei.com/fxrate').then(
       data => {
@@ -25,6 +27,9 @@ function App() {
   })
   return (
     <div className="App">
+      <div>
+        <TextField id="standard-basic" label="Base" variant="standard" />
+      </div>
       {rates}
       <Button variant="contained">Hello World</Button>
     </div>
